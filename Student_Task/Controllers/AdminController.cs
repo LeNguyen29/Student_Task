7947ee3e34c29178147f9de7f62319fe34f09efb
Lễ Nguyễn <nguyenbale29@gmail.com>
2024-01-03 23:23:44 +0700

    Attempting to add accounts management

---
+++ Student_Task/Controllers/AdminController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Student_Task.Data;
+
+namespace Student_Task.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            LoginDataAccessLayer loginDataAccessLayer = new LoginDataAccessLayer();
+
+            return View(loginDataAccessLayer.GetAllAccounts());
+        }
+
+        // GET: AdminController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Student_Task/Data/LoginDataAccessLayer.cs
+++ Student_Task/Data/LoginDataAccessLayer.cs
@@ -8,6 +8,39 @@ namespace Student_Task.Data
     {
         string connectionString = DatabaseContext.CString;
 
+        public List<LoginModel>? GetAllAccounts()
+        {
+            List<LoginModel> accounts = new List<LoginModel>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM mst_Account";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        accounts.Add(new LoginModel()
+                        {
+                            LoginId = reader["LoginId"].ToString(),
+                            UserName = reader["UserName"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            Email = reader["Email"].ToString(),
+                            Role = reader["Role"].ToString()
+                        });
+                    }
+                }
+                else
+                    return null;
+            }
+
+            return accounts;
+        }
+
         public LoginModel? CheckLogin(string loginID, string passwordHash)
         {
             LoginModel loginUser = new LoginModel();
